6eb20a8ab1b857cbc3d9fadc234a3f5822765367
Nellipro <nellifrasen@gmail.com>
2026-08-24 12:41:59 +0200

    added a temp player and temp player movement

--- Assets/Code/Submarine/SubmarineMovement.cs
+++ Assets/Code/Submarine/SubmarineMovement.cs
@@ -2,15 +2,44 @@ using UnityEngine;
 
 public class SubmarineMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    public Transform thisShip;
+    public Rigidbody rb;
+
+    [Header("Movement Settings")]
+    public  float turnSpeed = 60f;
+    public float thrustSpeed = 45f;
+    public float boostSpeed = 100f;
+    public float boostFuel = 100f;
+    public float maxBoostFuel = 100f;
+    public float linDaming = 1f;
+    public float angDamping = 1f;
+    private float yaw;
+    private float pitch;
+    private float roll;
+    
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        
+        turn();
+        thrust();
+    }
+
+    void turn()
+    {
+        yaw = turnSpeed * Time.deltaTime * Input.GetAxis("Horizontal");
+        pitch = turnSpeed * Time.deltaTime * Input.GetAxis("Vertical");
+        roll = turnSpeed * Time.deltaTime * Input.GetAxis("Rotate");
+        thisShip.Rotate(pitch, yaw, roll);
+    }
+
+    void thrust()
+    {
+        rb.AddForce(thisShip.forward * thrustSpeed * Input.GetAxis("Throttle"));
     }
 }
